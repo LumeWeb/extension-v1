3c41b82012f858cc21e1fd2b2394f1703f832459
Derrick Hammer <derrick@derrickhammer.com>
2022-03-05 20:37:50 -0500

    *eslint

--- public/js/resolver.js
+++ public/js/resolver.js
@@ -1,23 +1,25 @@
 (async function () {
-    let targetUrl = new URL(decodeURI(window.location)).searchParams.get("url");
-    let response = await browser.runtime.sendMessage(undefined, {
-        action: "resolve",
-        url: new URL(decodeURI(window.location)).searchParams.get("url"),
-    });
+  let targetUrl = new URL(decodeURI(window.location)).searchParams.get("url");
+  // eslint-disable-next-line no-undef
+  let response = await browser.runtime.sendMessage(undefined, {
+    action: "resolve",
+    url: new URL(decodeURI(window.location)).searchParams.get("url"),
+  });
 
-    let location;
+  let location;
 
-    if (response) {
-        location = targetUrl.toString();
-    } else {
-        location = browser.runtime.getURL("/error.html");
-    }
+  if (response) {
+    location = targetUrl.toString();
+  } else {
+    // eslint-disable-next-line no-undef
+    location = browser.runtime.getURL("/error.html");
+  }
 
-    let el = document.getElementById("success");
-    el.classList.add(["animate__animated"]);
-    el.classList.add(["animate__bounceIn"]);
+  let el = document.getElementById("success");
+  el.classList.add(["animate__animated"]);
+  el.classList.add(["animate__bounceIn"]);
 
-    setTimeout(() => {
-        window.location.href = location;
-    }, 1000);
+  setTimeout(() => {
+    window.location.href = location;
+  }, 1000);
 })();
