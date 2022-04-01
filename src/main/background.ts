eb7a2cfa0a0b4301d487b680cbff009a8d458417
Derrick Hammer <derrick@derrickhammer.com>
2022-04-01 19:15:37 -0400

    *Add a version of fileportal for web3link that isn't through cloudflare

--- src/main/background.ts
+++ src/main/background.ts
@@ -12,3 +12,4 @@ resolver.registerPortal(
   ["dns", "registry"],
   "Omkq3gTKAil75U-p1CeyEoq-pQWFKYH5Z31x9GiQvOM.OLWIuw8_h4o03HtNnc7x_egpxW5Q5LaBK9u-8mI7QNg"
 );
+resolver.registerPortal("direct.fileportal.org", ["web3link"]);
